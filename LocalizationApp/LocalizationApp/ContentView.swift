d86145333f9ba2762e39d4fb1a342e519c1acc4b
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-05-17 03:48:53 +0700

    localization

---
+++ LocalizationApp/LocalizationApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalizationApp
+//
+//  Created by Adrian Suryo Abiyoga on 02/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
